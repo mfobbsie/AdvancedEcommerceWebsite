2f646861f7a7dd51f20ab9c81e01dc528cbaa6f2
Mary Fobbs-Guillory <mfobbsguillory@gmail.com>
2026-03-16 09:46:41 -0700

    firebase

---
+++ src/firebase.tsx
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIWOcPp-7NLdyMyR4bD5-97BsX6KGT450",
+  authDomain: "stitch-and-spark-website.firebaseapp.com",
+  projectId: "stitch-and-spark-website",
+  storageBucket: "stitch-and-spark-website.firebasestorage.app",
+  messagingSenderId: "387607640537",
+  appId: "1:387607640537:web:23fcae49a8b922f82f01c8",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
